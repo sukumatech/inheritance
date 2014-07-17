17b366b74e926a9da401caca398bcb3e52ba50e0
susan <mwauragerald@njuguna-2.local>
2014-07-17 23:53:55 +0300

    Initial Commit

---
+++ inheritance/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  inheritance
+//
+//  Created by susan on 17/07/14.
+//  Copyright (c) 2014 oho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
